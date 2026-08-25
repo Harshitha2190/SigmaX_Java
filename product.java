42be6f656496869be35869ab912658476d96aed2
Harshitha P <harshitha2190@gmail.com>
2026-08-25 14:41:17 +0530

    Add files via upload

---
+++ product.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class product{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of a:");
+        int a = sc.nextInt();
+        System.out.println("Enter value of b:");
+        int b = sc.nextInt();
+        int product = a*b;
+        System.out.println("The Product of a and b is: \n"+product);
+
+    }
+
+}
