0064a77b70275183c9a09764d717cca9afa50584
Harshitha P <harshitha2190@gmail.com>
2026-08-27 22:27:59 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,13 @@
+public class datatypes{
+    public static void main(String args[]){
+        byte b=8;
+        System.out.println(b);
+        char ch ='a';
+        System.out.println(ch);
+        boolean var=false;
+        float price=10.5f;
+        //long
+        //double
+        short n =240;
+    }
+}
