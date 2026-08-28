c1d39b668c53d726e64992ae86b941e827d489b6
Harshitha P <harshitha2190@gmail.com>
2026-08-28 20:39:47 +0530

    Add files via upload

---
+++ Odd_Even.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Odd_Even{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a value: ");
+        int a=sc.nextInt();
+        if(a%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
