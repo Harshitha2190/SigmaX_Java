d250527b8074a8c86b1e4519ac7670d119e34911
Harshitha P <harshitha2190@gmail.com>
2026-08-31 13:27:08 +0530

    Add files via upload

---
+++ PQ2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class PQ2{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Side value: ");
+        int s = sc.nextInt();
+        int area = s*s;
+        System.out.print("Area of Square: "+area);
+    }
+}
